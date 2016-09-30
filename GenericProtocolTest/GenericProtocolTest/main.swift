8e2f458a3594cfeace840b4d0afe9bc6cc795cf9
Zheng Xing <zhexing@cisco.com>
2016-09-30 15:05:33 +0800

    first commit

---
+++ GenericProtocolTest/GenericProtocolTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenericProtocolTest
+//
+//  Created by zhexing on 30/09/2016.
+//  Copyright © 2016 cisco.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
